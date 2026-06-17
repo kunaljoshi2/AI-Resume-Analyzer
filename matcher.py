db1be21f00ce16e7f2d55b7351ca5a2629c78798
Kunal Joshi <115833735+kunaljoshi2@users.noreply.github.com>
2026-06-17 12:42:55 -0700

    Add lemmatization using spaCy and update stop words.

--- matcher.py
+++ matcher.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, ENGLISH_STOP_WORDS
 from sklearn.metrics.pairwise import cosine_similarity
+import spacy, re
 
+nlp = spacy.load("en_core_web_sm")
 
 customStopWords = list(ENGLISH_STOP_WORDS) + [
     "bonus", "candidate", "ideal", "looking", "required",
@@ -12,12 +14,31 @@ customStopWords = list(ENGLISH_STOP_WORDS) + [
     "perform", "execute", "handle", "oversee", "contribute",
     "position", "responsibilities", "qualifications", "requirements",
     "duties", "tasks", "work", "job", "career", "company", "team",
-    "member", "environment", "culture", "using"
+    "member", "environment", "culture", "using", "alongside", "participate",
+    "real", "world", "person", "multi", "additionally", "fundamental", "proven",
+    "portfolio", "possess", "include", "write", "logs", "audit", "managing"
 ]
+
+def lemmatization(text):
+
+    #cleaning
+    text = re.sub(r'([.!?,()\-])', r' \1 ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    doc = nlp(text)
+
+    lemmas = [token.lemma_.lower() for token in doc if not token.is_punct and not token.is_space]
+
+    return " ".join(lemmas)
+
 
 
 
 def calculateMatchScores(resumeText, jobDescription):
+
+    resumeText = lemmatization(resumeText)
+    jobDescription = lemmatization(jobDescription)
+
     if not resumeText or not jobDescription:
         return 0.0
     
@@ -35,6 +56,9 @@ def calculateMatchScores(resumeText, jobDescription):
 
 
 def extractKeywords(resumeText, jobDescription):
+
+    resumeText = lemmatization(resumeText)
+    jobDescription = lemmatization(jobDescription)
 
     corpus = [resumeText, jobDescription]
 
@@ -58,3 +82,7 @@ def extractKeywords(resumeText, jobDescription):
                 missing.append(word)
 
     return matched, missing
+
+
+
+
